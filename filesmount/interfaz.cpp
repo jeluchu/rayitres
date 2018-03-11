89cb714844f4a47f9dcb09827121c08ac3b0c408
Jeluchu <jesusmarcalderon@gmail.com>
2018-03-11 15:28:04 +0100

    NEWS

--- filesmount/interfaz.cpp
+++ filesmount/interfaz.cpp
@@ -70,7 +70,7 @@ void inicio(){
 void jugadorDEF() {
 
   int jugador=1,error,empate=0,ganar=0;
-  char c1='1',c2='2',c3='3',c4='4',c5='5',c6='6',c7='7',c8='8',c9='9',tiro,marca,respuesta='y';
+  char posicion_1='1',posicion_2='2',posicion_3='3',posicion_4='4',posicion_5='5',posicion_6='6',posicion_7='7',posicion_8='8',posicion_9='9',tiro,marca,respuesta='y';
 
   do{
 
@@ -81,11 +81,11 @@ void jugadorDEF() {
        system("toilet -fpagga RAYITRES\n");
 
        printf("┏━━━┳━━━┳━━━┓\n");
-       printf("┃ %c ┃ %c ┃ %c ┃\n",c1,c2,c3);
+       printf("┃ %c ┃ %c ┃ %c ┃\n",posicion_1,posicion_2,posicion_3);
        printf("┣━━━╋━━━╋━━━┫\n");
-       printf("┃ %c ┃ %c ┃ %c ┃\n",c4,c5,c6);
+       printf("┃ %c ┃ %c ┃ %c ┃\n",posicion_4,posicion_5,posicion_6);
        printf("┣━━━╋━━━╋━━━┫\n");
-       printf("┃ %c ┃ %c ┃ %c ┃\n",c7,c8,c9);
+       printf("┃ %c ┃ %c ┃ %c ┃\n",posicion_7,posicion_8,posicion_9);
        printf("┗━━━┻━━━┻━━━┛\n");
 
 
@@ -106,55 +106,55 @@ void jugadorDEF() {
 
        scanf("%c",&tiro);
 
-       if (tiro=='1' && c1=='1') {c1=marca;}
-       else if (tiro=='2' && c2=='2') {c2=marca;}
-       else if (tiro=='3' && c3=='3') {c3=marca;}
-       else if (tiro=='4' && c4=='4') {c4=marca;}
-       else if (tiro=='5' && c5=='5') {c5=marca;}
-       else if (tiro=='6' && c6=='6') {c6=marca;}
-       else if (tiro=='7' && c7=='7') {c7=marca;}
-       else if (tiro=='8' && c8=='8') {c8=marca;}
-       else if (tiro=='9' && c9=='9') {c9=marca;}
+       if (tiro=='1' && posicion_1=='1') {posicion_1=marca;}
+       else if (tiro=='2' && posicion_2=='2') {posicion_2=marca;}
+       else if (tiro=='3' && posicion_3=='3') {posicion_3=marca;}
+       else if (tiro=='4' && posicion_4=='4') {posicion_4=marca;}
+       else if (tiro=='5' && posicion_5=='5') {posicion_5=marca;}
+       else if (tiro=='6' && posicion_6=='6') {posicion_6=marca;}
+       else if (tiro=='7' && posicion_7=='7') {posicion_7=marca;}
+       else if (tiro=='8' && posicion_8=='8') {posicion_8=marca;}
+       else if (tiro=='9' && posicion_9=='9') {posicion_9=marca;}
        else {printf("¡Error! Este movimiento no está permitido\n"); error=2;}
 
-       if(c1=='X'||c1=='O')
+       if(posicion_1=='X'||posicion_1=='O')
 
        {
 
-         if(c2==c1&&c3==c1)
+         if(posicion_2==posicion_1&&posicion_3==posicion_1)
           {
            ganar=1;
           }
 
-         if(c4==c1&&c7==c1)
+         if(posicion_4==posicion_1&&posicion_7==posicion_1)
           {
            ganar=1;
           }
        }
 
-       if(c5=='X'||c5=='O')
+       if(posicion_5=='X'||posicion_5=='O')
        {
-                if (c1 == c5 && c9 == c5)
+                if (posicion_1 == posicion_5 && posicion_9 == posicion_5)
                         {ganar=1;}
-                if (c2 == c5 && c8 == c5)
+                if (posicion_2 == posicion_5 && posicion_8 == posicion_5)
                         {ganar=1;}
-                if (c4 == c5 && c6 == c5)
+                if (posicion_4 == posicion_5 && posicion_6 == posicion_5)
                         {ganar=1;}
-                if (c3 == c5 && c7 == c5)
+                if (posicion_3 == posicion_5 && posicion_7 == posicion_5)
                         {ganar=1;}
          }
 
-       if(c9=='X'||c9=='O')
+       if(posicion_9=='X'||posicion_9=='O')
        {
-                  if (c6 == c9 && c3 == c9)
+                  if (posicion_6 == posicion_9 && posicion_3 == posicion_9)
                           {ganar=1;}
-                  if (c7 == c9 && c8 == c9)
+                  if (posicion_7 == posicion_9 && posicion_8 == posicion_9)
                          {ganar=1;}
        }
 
-       if ((c1 != '1' && c2 != '2' && c3 != '3' &&
-           c4 != '4' && c5 != '5' && c6 != '6' &&
-           c7 != '7' && c8 != '8' && c9 != '9') &&
+       if ((posicion_1 != '1' && posicion_2 != '2' && posicion_3 != '3' &&
+           posicion_4 != '4' && posicion_5 != '5' && posicion_6 != '6' &&
+           posicion_7 != '7' && posicion_8 != '8' && posicion_9 != '9') &&
            ganar == 0)
        {
            empate=1;
@@ -171,11 +171,11 @@ void jugadorDEF() {
            {
                printf("Jugador: %d ¡GANA!\n\n", jugador);
                printf("┏━━━┳━━━┳━━━┓\n");
-               printf("┃ %c ┃ %c ┃ %c ┃\n", c1,c2,c3);
+               printf("┃ %c ┃ %c ┃ %c ┃\n", posicion_1,posicion_2,posicion_3);
                printf("┣━━━╋━━━╋━━━┫\n");
-               printf("┃ %c ┃ %c ┃ %c ┃\n", c4,c5,c6);
+               printf("┃ %c ┃ %c ┃ %c ┃\n", posicion_4,posicion_5,posicion_6);
                printf("┣━━━╋━━━╋━━━┫\n");
-               printf("┃ %c ┃ %c ┃ %c ┃\n", c7,c8,c9);
+               printf("┃ %c ┃ %c ┃ %c ┃\n", posicion_7,posicion_8,posicion_9);
                printf("┗━━━┻━━━┻━━━┛\n");
            }
 
@@ -183,11 +183,11 @@ void jugadorDEF() {
            {
                printf("Empate %c!\n", jugador);
                printf("┏━━━┳━━━┳━━━┓\n");
-               printf("┃ %c ┃ %c ┃ %c ┃\n",c1,c2,c3);
+               printf("┃ %c ┃ %c ┃ %c ┃\n",posicion_1,posicion_2,posicion_3);
                printf("┣━━━╋━━━╋━━━┫\n");
-               printf("┃ %c ┃ %c ┃ %c ┃\n",c4,c5,c6);
+               printf("┃ %c ┃ %c ┃ %c ┃\n",posicion_4,posicion_5,posicion_6);
                printf("┣━━━╋━━━╋━━━┫\n");
-               printf("┃ %c ┃ %c ┃ %c ┃\n",c7,c8,c9);
+               printf("┃ %c ┃ %c ┃ %c ┃\n",posicion_7,posicion_8,posicion_9);
                printf("┗━━━┻━━━┻━━━┛\n");
            }
 
@@ -199,15 +199,15 @@ void jugadorDEF() {
                    empate=0;
                    jugador=2;
                    ganar=0;
-                   c1='1';
-                   c2='2';
-                   c3='3';
-                   c4='4';
-                   c5='5';
-                   c6='6';
-                   c7='7';
-                   c8='8';
-                   c9='9';
+                   posicion_1='1';
+                   posicion_2='2';
+                   posicion_3='3';
+                   posicion_4='4';
+                   posicion_5='5';
+                   posicion_6='6';
+                   posicion_7='7';
+                   posicion_8='8';
+                   posicion_9='9';
            }
        }
 
